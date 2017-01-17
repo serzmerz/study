bdb164881a42a45cb10139721277a36ddcbbea61
Sergey Tanskiy <serzmerz@gmail.com>
2017-01-17 21:45:38 +0200

    add inheritance

--- Student.php
+++ Student.php
@@ -1,30 +1,7 @@
 <?php
-class Student {
+require_once ('Person.php');
+class Student extends Person {
     private $speciality;
-    private $name;
-    private $age;
-
-    /**
-     * @param mixed $name
-     */
-    public function setName($name)
-    {
-        $this->name = $name;
-    }
-
-    /**
-     * @param mixed $age
-     */
-    public function setAge($age)
-    {
-        $this->age = $age;
-    }
-    public function getName(){
-        return $this->name;
-    }
-    public function getAge(){
-        return $this->age;
-    }
     public function __construct($name,$age,$speciality)
     {
         $this->setName($name);
